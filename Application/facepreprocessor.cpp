dea4907f7773dfbe0fa1add6466477d44132ba20
Kovács Balázs <kovacs.balazs.e@gmail.com>
2015-03-31 13:02:42 +0200

    convert to grayscale + normalization

--- Application/facepreprocessor.cpp
+++ Application/facepreprocessor.cpp
@@ -1,15 +1,21 @@
 #include "facepreprocessor.h"
 
+#include <opencv2/imgproc/imgproc.hpp>
+
 using namespace cv;
 
 FacePreprocessor::FacePreprocessor(const Mat& input)
-    : input(input)
+    : input(input),
+      result(Mat(input.rows, input.cols, CV_8UC1))
 {
 
 }
 
 Mat FacePreprocessor::Preprocess() throw (NoFaceFoundException)
 {
-    result = input;
+    cvtColor(input, result, CV_BGR2GRAY);
+    Mat normalized(result.rows, result.cols, CV_8UC1);
+    normalize(result, normalized, 0.0, 255.0, NORM_MINMAX);
+
     return result;
 }
